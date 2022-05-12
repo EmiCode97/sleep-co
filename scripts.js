a3c14c07b9c5b0619daeec2ac22934cf401017ca
EmiCode97 <87772887+EmiCode97@users.noreply.github.com>
2022-05-12 17:27:13 +0200

    lightgallery

--- scripts.js
+++ scripts.js
@@ -14,3 +14,20 @@ AOS.init();
 
 
 });
+
+
+
+
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false
+  
+});
+
+
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
+  
+});
