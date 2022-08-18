5cac2e9906e2d8d036a6ff1a20e44ba451e665f9
EmiCode97 <87772887+EmiCode97@users.noreply.github.com>
2022-08-18 18:12:47 +0200

    cookie

--- _includes/personal-tracking-scripts.js
+++ _includes/personal-tracking-scripts.js
@@ -1,9 +1,9 @@
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){window.dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'GA_MEASUREMENT_ID');
-</script> 
+
 
 
